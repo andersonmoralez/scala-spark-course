88c7aecad2bbcd5b9b590012db0a9e007afe1bbb
Anderson Moralez <anderson.moralez@hotmail.com>
2026-05-20 22:04:01 -0300

    Aula 12# Window Functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,9 @@
 package br.andersonmoralez.sparkvideocourse
 
-import br.andersonmoralez.sparkvideocourse.io.DataLoader
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.types.StringType
+import io.DataLoader
+
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.row_number
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -28,25 +29,16 @@ object Main {
         println("Dados carregados com sucesso!")
       }
 
-
       //df.printSchema() // Mostra a estrutura (colunas e tipos)
       //df.show(10) // Mostra as 10 primeiras linhas
 
-      //df.select("Age", "Gender", "Avg_BPM").show()
-      //val column = df("Age")
-      col("Age")
       import spark.implicits._
-      $"Age"
 
-      //df.select(column, $"Gender", df("Avg_BPM")).show()
-
-      val column = df("Age")
-      val newColumn = (column + 2.0).as("Age_2.0")
-      val columnString = column.cast(StringType).as("Age_String_Type")
-
-      df.select(column, newColumn, columnString)
-        .filter(newColumn > 20.00)
-        .filter(newColumn > column)
+      //Particiona por Frequência de Refeição Diária e ordena por Calorias Perdidas
+      val window = Window.partitionBy($"Daily meals frequency".as("dailyMealsFrequency")).orderBy($"Calories_Burned".desc)
+      df.withColumn("rn", row_number.over(window))
+        .filter($"rn" === 1) // Filtra frequência única
+        .sort($"Calories_Burned".desc)// Ordena pela maior caloria perdida
         .show()
 
     } catch {
